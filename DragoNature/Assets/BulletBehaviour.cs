42bd14189b786e833b3dc1a06093f105536e613a
Vinicius Hashirama <vinicius_hashirama@outlook.com>
2019-08-30 01:22:50 -0300

    Weapon System

---
+++ DragoNature/Assets/BulletBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float speed = 20f;
+
+    public float damage = 100f;
+    public Rigidbody rb{
+        get{
+            if(m_rb == null)
+                m_rb = GetComponent<Rigidbody>();
+                return m_rb;
+        }
+    }
+    protected Rigidbody m_rb;
+
+    private void Start() {
+        rb.velocity = transform.forward * speed;     
+    }
+}
