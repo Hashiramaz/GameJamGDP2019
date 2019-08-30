de50c6432a38f949236099fe1d4672f1ef9cdcf0
Vinicius Hashirama <vinicius_hashirama@outlook.com>
2019-08-30 20:18:45 -0300

    Enemy Run and Gun

---
+++ DragoNature/Assets/EnemyWeaponRoutine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeaponRoutine : MonoBehaviour
+{
+public Transform bulletpoint;
+public GameObject bulletprefab;
+
+public ParticleSystem muzzleFlash;
+
+public bool canShoot;
+
+public float firingRate = 0.5f;
+
+private void Start() {
+    StartShoot();
+}
+public void StartShoot(){
+canShoot = true;
+StartCoroutine(WeaponRoutine());
+}
+
+public void StopShoot(){
+    canShoot = false;
+}
+
+
+    IEnumerator WeaponRoutine(){
+        while (canShoot)
+        {
+            
+            yield return new WaitForSeconds(firingRate);
+            Shoot();
+        }
+            
+        
+    }
+
+    public void Shoot(){
+        Instantiate(bulletprefab,bulletpoint.position,bulletpoint.rotation);
+        
+        if(muzzleFlash != null)
+            muzzleFlash.Play();
+    }
+}
