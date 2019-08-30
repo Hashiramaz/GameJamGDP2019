25d65c9521f3b9f138a683f5b0800ffa1f04c533
Vinicius Hashirama <vinicius_hashirama@outlook.com>
2019-08-30 04:54:50 -0300

    added jmo assets

---
+++ DragoNature/Assets/Scripts/Utility/AutoDestroyInSeconds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyInSeconds : MonoBehaviour
+{
+   public float timeToDestroy;
+
+private void Start() {
+    
+StartCoroutine(AutoDestroyIn(timeToDestroy));
+}
+
+
+
+   IEnumerator AutoDestroyIn(float _time){
+       yield return new WaitForSeconds(_time);
+       transform.SendMessage("Die");
+       Destroy(gameObject);
+   }
+}
