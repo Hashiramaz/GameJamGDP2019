6dc3eaa2cf0e63c636e98001b922d6eda8ee059b
Vinicius Hashirama <vinicius_hashirama@outlook.com>
2019-08-30 17:51:56 -0300

    End Game Condition

---
+++ DragoNature/Assets/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float health = 1f;
+    public  bool allowDebug = true   ;
+
+
+    
+    public void TakeDamage(float amount){
+        health -= amount;
+        
+        if(health <= 0)
+            PlayerDies();
+    }
+
+    public void PlayerDies(){
+        GameManager.instance.StopGame();
+    }
+    
+    
+    private void OnGUI() {
+        
+        if(!allowDebug)
+            return;
+        
+        GUI.Label(new Rect(10, 300, 200, 200), "Actual Life: " + health);
+    }
+
+}
---
+++ DragoNature/Assets/UiManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UiManager : MonoBehaviour
+{
+    public static UiManager instance;
+
+    public GameObject finalScreen;
+    public GameObject scoreInformations;
+
+    private void Awake() {
+        if(instance == null)
+            instance = this;
+    }
+
+    public void EnableFinalScreen(){
+        finalScreen.SetActive(true);
+        scoreInformations.SetActive(false);
+    }
+
+    public void ReturnToMenu(){
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ResetGame(){
+        GameManager.instance.ResetGame();
+    }
+}
