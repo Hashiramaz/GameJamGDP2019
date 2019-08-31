1516a6a4605cb45325e0368b2ccf5aa0036beb4c
Vinicius Hashirama <vinicius_hashirama@outlook.com>
2019-08-31 16:17:22 -0300

    backup Commit

---
+++ DragoNature/Assets/FinalSceneScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FinalSceneScript : MonoBehaviour
+{
+    public void BackToMenu(){
+        SceneManager.LoadScene("Menu");
+    }
+}
