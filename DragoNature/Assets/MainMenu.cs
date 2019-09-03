e9d38a11ab22a113dfa8df74a7edea0eb68d8b06
Vinicius Hashirama <vinicius_hashirama@outlook.com>
2019-09-03 16:21:21 -0300

    final commit

--- DragoNature/Assets/MainMenu.cs
+++ DragoNature/Assets/MainMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     public void PlayGame(){
         SceneManager.LoadScene("Game");
@@ -12,7 +13,7 @@ public class MainMenu : MonoBehaviour
     public void QuitGame(){
         Application.Quit();
     }
-
+    
     
 }
 
